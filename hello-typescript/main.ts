77e99c5f80e32320214e5d326e4cbe32618e087f
Jhonata Galante <jhonata.galante@gmail.com>
2020-09-19 07:38:28 -0300

    Adicionado módulos de 01 ao 06 do curso da loiane.training

---
+++ hello-typescript/main.ts
@@ -0,0 +1,25 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y){
+    return x + y;
+}
+
+//ES 6
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(v){
+    return v * 2;
+});
+
+numeros.map( v => v * 2);
+
+class Matematica {
+    soma(x,y){
+        return x + y;
+    }
+}
+
+var n1 = 'sdadsada';
+n1 = 4;
